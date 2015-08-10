03cd1d39e533f1dd7d640695f1abe52d7d3665ad
andi9010 <andres_solarteo@hotmail.com>
2015-08-10 15:19:18 -0500

    configuracion inicial

---
+++ Client/Scripts/_references.js
@@ -0,0 +1,4 @@
+﻿/// <reference path="../bower_components/angular/angular.js" />
+/// <reference path="../bower_components/angular-animate/angular-animate.js" />
+/// <reference path="../bower_components/angular-aria/angular-aria.js" />
+/// <reference path="../bower_components/angular-material/angular-material.js" />
